ef5704b5f0aa2520e0d87be3b0d05858d4206163
Numair <Numair2644@protonmail.com>
2023-10-09 00:29:13 -0400

    allocate mem buffer based on file size

--- cat.c
+++ cat.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <errno.h>
 
-void print_file(FILE *stream)
+void print_file(FILE *stream, long file_size)
 {
-    // 10 KiB input, TODO this is obviously insufficient for bigger files
-    char buf[10 * 1024];
-    while (fgets(buf, sizeof(buf), stream)) {
+
+    char *buf = malloc(file_size);
+    while (fgets(buf, sizeof(buf), stream))
         printf("%s", buf);
-    }
+
+    free(buf);
 }
 
 int main(int argc, const char *argv[])
 {
     if (argc > 1) {
         for (int i = 1; i < argc; ++i) {
+            // calculate file size in bytes to be dynamically allocated
             FILE *fname = fopen(argv[i], "r");
-            print_file(fname);
+            fseek(fname, 0, SEEK_END);
+            long file_size = ftell(fname);
+            fseek(fname, 0, SEEK_SET);
+
+            print_file(fname, file_size);
             if (errno == 0) 
                 fclose(fname);
             else
                 perror(argv[0]);
         }
     } else {
-        print_file(stdin);
+        // 10 KiB for stdin
+        print_file(stdin, 1024 * 10);
     }
     return 0;
 }
